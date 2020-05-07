ba2b511bf83d3bf31ed93672ba2b233b4ca0020a
Anas Khmammash <anasfortnite@yahoo.com>
2020-05-07 21:41:05 +0300

    added code for moving bat, moving ball, and when game is over.

--- bat_ball.py
+++ bat_ball.py
@@ -4,10 +4,10 @@ import time
 canvasWidth = 750
 canvasHeight = 500
 window = tkinter.Tk()
-canvas = tkinter.Canavas(window, width=canvasWidth, height=canvasHeight, bg="dodgerblue4")
+canvas = tkinter.Canvas(window, width=canvasWidth, height=canvasHeight, bg="dodgerblue4")
 canvas.pack()
 
-bat = canvas.create_rectangle(0, 0, 40, 10, fill = "dark turqoise")
+bat = canvas.create_rectangle(0, 0, 40, 10, fill = "dark turquoise")
 ball = canvas.create_oval(0, 0, 10, 10, fill = "deep pink")
 
 windowOpen = True
@@ -21,21 +21,62 @@ def main_loop():
 		if windowOpen == True:
 			check_game_over()
 
-
 leftPressed = 0
 rghitPressed = 0
 
 def on_key_pressed(event):
-	global leftPressed, righttPressed
+	global leftPressed, rightPressed
 	if event.keysym == "Left":
 		leftPressed = 1
 	elif event.keysym == "Right":
 		rightPressed = 1
-
-
+   
 def on_key_release(event):
-	global leftPressed, RightPressed
+	global leftPressed, rightPressed
 	if event.keysym == "Left":
-		LeftPressed = 0
+		leftPressed = 0
 	elif event.keysym == "right":
 		rightPressed = 0
+
+batSpeed = 6
+def move_bat():
+	batMove = batSpeed*rightPressed - batSpeed*leftPressed 
+	(batLeft, batTop, batRight, batBottom) = canvas.coords(bat)
+	if (batLeft > 0) and (batRight < canvasWidth or batMove < 0):
+		canvas.move(bat, batMove, 0)
+
+ballMoveX = 4
+ballMoveY = -4
+setBatTop = canvasHeight-40
+setBatBottom = canvasHeight-30
+
+def move_ball():
+	global ballMoveX, ballMoveY
+	(ballLeft, ballTop, ballRight, ballBottom) = canvas.coords(ball)
+	if ballMoveX > 0 and ballRight > canvasWidth:
+		ballMovex = -ballMoveX
+	if ballMoveX < 0 and ballLeft < 0:
+		balMoveX = -ballMoveX
+	if ballMoveY < 0 and ballTop < 0:
+		ballMoveY = -ballMoveY
+	if ballMoveY > 0 and ballBottom > setBatTop and ballBottom < setBatBottom:
+		(batLeft, batTop, batRight, batBottom) = canvas.coords(bat)
+		if ballRight > batLeft and ballLeft < batRight:
+			ballMoveY = -ballMoveY
+	canvas.move(ball, ballMovex, ballMoveY)
+
+def check_game_over():
+	(batLeft, ballTop, ballBottom) = canvas.coords(ball)
+	if ballTop > canvasHeight:
+		playAgain = tkinter.messagebox.askesno(message = "Do you want to play again?" )
+		if playAgain == True:
+			reset()
+		else:
+			close()
+
+def close():
+	global windowOpen
+	windowOpen = False
+	window.destroy()
+
+
